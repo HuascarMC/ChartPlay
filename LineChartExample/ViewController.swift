c7751c45577b8295d046d4f7a0b8a8f7c7a67406
Huascar <huascarmc@hotmail.com>
2018-06-05 15:35:04 +0100

    graph displays succesfully

--- LineChartExample/ViewController.swift
+++ LineChartExample/ViewController.swift
@@ -29,6 +29,28 @@ class ViewController: UIViewController {
         let txt = txtTextBox.text
         let value = (txt! as NSString).doubleValue
         self.numbers.append(value)
+        print(value.description)
+        self.updateGraph()
+    }
+    
+    private func updateGraph() {
+        var lineChartEntry = [ChartDataEntry]()
+        for i in 0..<numbers.count {
+            let value = ChartDataEntry(x: Double(i), y: numbers[i])
+            
+            lineChartEntry.append(value)
+        }
+        
+        let line1 = LineChartDataSet(values: lineChartEntry, label: "Number")
+        
+        line1.colors = [NSUIColor.blue]
+        
+        let data = LineChartData()
+        
+        data.addDataSet(line1)
+        
+        chtChart.data = data
+        chtChart.chartDescription?.text = "My awesome chart"
     }
     
 }
